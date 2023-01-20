99c51391cd4a7fda713774e1d23fb2a845650b7d
Juhee Kim <rlawngml59@gmail.com>
2023-01-20 16:23:29 +0900

    - stopwatch - 버튼 바꾸기

--- project/app_venv/MyApp/myapp/firstapp/urls.py
+++ project/app_venv/MyApp/myapp/firstapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
+
+app_name = 'mapPage'
 
 urlpatterns = [
-    path('', views.mapPage, name='mapPage'),
+    path('', mapPage, name='mapPage'),
 ]
